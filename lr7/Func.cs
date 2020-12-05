5de0d27d4409f2f1ced0dd4522594a8bb8fa2b69
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2020-12-05 19:25:18 +0300

    lr7

---
+++ lr7/Func.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr_7
+{
+    class Geometry
+    {
+        private double a, b, i, l;
+        private Geometry() { }
+        public Geometry(double A, double B) 
+        {
+            a = A;
+            b = B;
+        }
+
+        public double GetIntegral()
+        {
+            return
+            i = Math.Pow(b, 2)/2 - Math.Pow(a, 2)/2;
+        }
+
+        public double GetLgth()
+        {
+            return
+            l = Math.Sqrt(Math.Pow(b - a, 2) + Math.Pow(b - a, 2));
+        }
+ 
+        public void Load()
+        {
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+        }
+        public static Geometry LoadNew() 
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            return new Geometry(x, y);
+        }
+        public void Info()
+        {
+            Console.WriteLine(string.Format("a = {0} \nb = {1}", a, b));
+            Console.WriteLine(string.Format("Интеграл = {0:0.00}", GetIntegral()));
+            Console.WriteLine(string.Format("Длина отрезка a, y(a) до b, y(b) = {0:0.00}", GetLgth()));
+        }
+        public void Info(ConsoleColor bcgrc)
+        {
+            Console.BackgroundColor = bcgrc;
+            Console.Clear();
+            Info();
+        }
+
+    }
+}
---
+++ lr7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace lr_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Geometry int1, int2;
+
+#if !DEBUG
+            int1 = Geometry.LoadNew();
+            int1.Info();
+            int2 = new Geometry(2, 4);
+            int2.Info();
+#endif
+#if DEBUG
+            int2 = new Geometry(5, 7);
+            int2.Info(ConsoleColor.Green);
+#endif
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+    }
+
+}
