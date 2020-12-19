52f4eb7e30ab1dbd80a385901f046c372554281d
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2020-12-19 03:34:45 +0300

    lr10

---
+++ lr10/Class.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr10
+{
+    class Matrix
+    {
+        private float[,] mat;
+        int m, n;
+        public Matrix() { }
+        public void GenerateMat(int M, int N) 
+        {
+            m = M; n = N;
+            Random r = new Random(DateTime.Now.Millisecond);
+            mat = new float[M, N];
+            for (int i = 0; i < M; i++)
+                for (int j = 0; j < N; j++)
+                    mat[i, j] = (float)r.Next(1000) / 973f;
+        }
+
+        public void SaveMat(string pFileName) 
+        {
+            if (mat.Length > 0)
+            {
+                if (File.Exists(pFileName))
+                    File.Delete(pFileName);
+
+                FileInfo f = new FileInfo(pFileName);
+                TextWriter tw = f.CreateText();
+
+                tw.WriteLine(m.ToString());
+                tw.WriteLine(n.ToString());
+
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                        tw.WriteLine(i.ToString() + " " + j.ToString() + " " + mat[i, j].ToString("E10"));
+                tw.Close();
+            
+            }
+        }
+
+        public float SummMat(string pFileName) 
+        {
+            float sub = 0;
+            if (File.Exists(pFileName))
+            {
+                TextReader tr = File.OpenText(pFileName);
+                m = Convert.ToInt32(tr.ReadLine());
+                n = Convert.ToInt32(tr.ReadLine());
+                
+                mat = new float[m, n];
+                string line;
+                string[] substring;
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        line = tr.ReadLine();
+                        substring = line.Split(new char[] { ' ' }, 3);
+                        mat[i, j] = Convert.ToSingle(substring[2]);
+                    }
+
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        sub += mat[i, j];
+                    }
+
+                        tr.Close();
+                return sub;
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ lr10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            float x, x2;
+            Matrix m = new Matrix();
+            m.GenerateMat(10, 5);
+            m.SaveMat("FileForFirstMatrix.txt");
+            x = m.SummMat("FileForFirstMatrix.txt");
+           
+
+            Matrix m2 = new Matrix();
+            m2.GenerateMat(7, 6);
+            m2.SaveMat("FileForSecondMatrix.txt");
+            x2 = m.SummMat("FileForSecondMatrix.txt");
+
+            Console.WriteLine("Разность сумм элементов матриц = {0}", (x + x2));
+            Console.ReadKey();
+        }
+    }
+}
