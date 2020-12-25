e97d4ceb6dd7685322b568c3453806d717486dca
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2020-12-25 15:04:00 +0000

    lr11

---
+++ lr11/Class.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lr11
+{
+    class Person
+    {
+        public String ID { get; set; }
+        public String LastName { get; set; }
+        public String FirstName { get; set; }
+        public String email { get; set; }
+        public float salary { get; set; }
+        public String Skill { get; set; }
+        public float Tax { get; set; }
+
+        public static Person Load(String s)
+        {
+            while (s.IndexOf(",,") > 0)
+            {
+                s = s.Insert((s.IndexOf(",,") + 1), "None");
+            }
+            Person p = new Person();
+            string[] elem = s.Split(',');
+            p.ID = elem[0].Trim();
+            p.LastName = elem[1].Trim();
+            p.FirstName = elem[2].Trim();
+            p.email = elem[3].Trim();
+            p.salary = Convert.ToSingle(elem[4].TrimStart('$').Replace('.', ','));
+            p.Skill = elem[5].Trim();
+            p.Tax = Convert.ToSingle(elem[6].TrimStart('$').Replace('.', ','));
+            return p;
+        }
+
+        public override string ToString()
+        {
+            String y = string.Format
+                (
+                "*******************\n" +
+                "ID: {0}, Lastname: {1}, Name: {2}\n" +
+                "E-mail: {3}, Salary: {4}, Skill: {5}, Tax: {6}",
+                ID, LastName, FirstName, email, salary, Skill, Tax
+                );
+            return y;
+        }
+
+    }
+}
---
+++ lr11/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lr11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader f_in = new StreamReader("lr11_25.csv");
+
+#if !DEBUG 
+            TextWriter save_out = Console.Out;
+            var new_out = new StreamWriter(@"lr11_output.txt");
+            Console.SetOut(new_out);
+#endif
+            List<Person> all = new List<Person>();
+
+                String line = f_in.ReadLine();
+                while ((line = f_in.ReadLine()) != null)
+                {
+                    all.Add(Person.Load(line));
+                }
+
+            /*Console.WriteLine("All users: {0}", all.Count);
+            foreach (var p in all)
+                Console.WriteLine(p);*/
+
+
+            Console.WriteLine("*** Task 1 ***");
+            float max = 0;
+            Person rich = new Person();
+            foreach (var m in all)
+            {
+                
+                float t = m.salary - m.Tax;
+                if (max < t)
+                { max = t;
+                    rich = m;
+                }
+            }
+            Console.WriteLine("Сотрудник с максимальной зарплатой после уплаты налога:\n{0}\n", rich);
+            float min = max;
+            Person poor = new Person();
+            foreach (var n in all)
+            {
+
+                float t2 = n.salary - n.Tax;
+                if (min > t2)
+                {
+                    min = t2;
+                    poor = n;
+                }
+            }
+            Console.WriteLine("Сотрудник с минимальной зарплатой после уплаты налога:\n{0}\n", poor);
+
+
+            Console.WriteLine("*** Task 2 ***");
+            float sal = (from p in all
+                          where p.Skill != "None"
+                          select p.salary).Average();
+            Console.WriteLine("Средняя з/п сотрудников без указанного навыка: {0}\n", sal);
+
+
+            Console.WriteLine("*** Task 3 ***");
+            int mail = all.FindAll(p => p.email != "None").ToList().Count;
+            Console.WriteLine("Количество сотрудников с e-mail: {0}\n", mail);
+
+
+            Console.WriteLine("*** Task 4 ***");
+            float salar = (from p in all
+                           select p.salary).Average();
+            int vse = all.Count();
+            Console.WriteLine("Чтобы рассчитать ОФЗП нужно среднюю з/п (${0}) " +
+            "умножить на кол-во сотрудников ({1}) и на 12 месяцев", salar, vse);
+            Console.WriteLine("Объем фонда заработной платы = {0}", salar*vse*12);
+
+
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
